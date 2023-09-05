16dbb5dc69fa1e4d708759128e1d423c7f9f9843
Samantha St-Germain <samantha.stgermain07@gmail.com>
2023-09-04 21:09:59 -0400

    created a useEffect function, added API endpoint, added prevent default

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import './App.css';
 import React, { useState, useEffect } from 'react';
+import './App.css';
+import Recipe from './Recipe';
 
 const App = () => {
   
@@ -9,7 +10,50 @@ const App = () => {
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState("");
   const [query, setQuery] = useState("");
-  const [randomQuery, setRandomQuery] = useState("");
+  useEffect(() => {
+    getIngredients();
+  }, [query])
+
+  const getIngredients = async () => {
+    const response = await fetch
+    ('http://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}');
+    const data = await response.json();
+    setRecipes(data.hits);
+  };
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  };
+
+  const getRecipe = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  }
+
+  return (
+    <div className="App">
+      <form className="searchForm" onSubmit={getRecipe} >
+        <label for="searchInput" className="sr-only">Search</label>
+        <input className="searchBar" id="searchInput" type="search" value={search}
+        onChange={updateSearch} />
+        <button className="bearchButton" type="submit" >
+          Search
+        </button>
+      </form>
+      <div className="recipes">
+        {recipes.map(recipe => (
+        <Recipe
+          key={recipe.recipe.label}
+          calories={recipe.recipe.calories}
+          image={recipe.image}
+          ingredients={recipe.recipe.ingredients}
+        />
+
+        ))}
+      </div>
+    </div>
+  );
 }
 
 
